c318658ae88d696ef7f68ecaa20b47a789b2b824
MarcoHuXHu <huxhu2333@gmail.com>
2017-10-13 22:09:59 +0800

    update for macro nutrition model

--- web/model.py
+++ web/model.py
@@ -1,24 +1,26 @@
 import asyncio, dao
 from dao import Model, IntegerField, StringField
 
-class User(Model):
-    __table__ = 'user'
+class Macro_Nutrition(Model):
+    __table__ = 'macro_nutrition'
 
-    id = IntegerField(primary_key=True, column_type='Int')
-    email = StringField(column_type='varchar(50)')
-    passwd = StringField(column_type='varchar(50)')
-    name = StringField(column_type='varchar(50)')
-    image = StringField(column_type='varchar(500)')
+    food_id = IntegerField(primary_key=True, column_type='int')
+    food_name = StringField(column_type='varchar(50)')
+    unit = StringField(column_type='int')
+    unit_name = StringField(column_type='varchar(50)')
+    energy = StringField(column_type='float')
+    carbohydrate = StringField(column_type='float')
+    protein = StringField(column_type='float')
+    fat = StringField(column_type='float')
 
 
 def test():
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(dao.create_connection(loop, db='awesome'))
+    loop.run_until_complete(dao.create_connection(loop, db='diet_pal'))
 
-    u = User(name='Test', email='test1@example.com', passwd='1234567890', image='about:blank')
+    u =  loop.run_until_complete(Macro_Nutrition.findByKey(1))
+    print(u)
 
-    loop.run_until_complete(u.save())
-    # rs = loop.run_until_complete(select('select * from user;', ()))
 
 
 
