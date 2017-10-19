c23dddf77a0d8c9bca684299ec7d6661b8ffcf7b
MarcoHuXHu <huxhu2333@gmail.com>
2017-10-19 21:22:03 +0800

    update controller for get method with parameters

--- web/controller.py
+++ web/controller.py
@@ -5,13 +5,16 @@ from webframe import get, post
 from model import Macro_Nutrition
 
 #编写用于测试的URL处理函数
-@get('/test')
-async def test(request):
-    body='<h1>Hello World</h1>'
+@get('/hello')
+async def hello(request):
+    user = 'World'
+    if 'user' in request.query:
+        user = request.query['user']
+    body = '<h1>Hello {0}!</h1>'.format(user)
     return body
 
-@get('/')
-async def index(request):
+@get('/test')
+async def test(request):
     foods = await Macro_Nutrition.find()
     return {
         '__template__': 'index.html',
