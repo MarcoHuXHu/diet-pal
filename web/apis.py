a675a1b84317c02f5158c2b54d9c29b92f8216b0
MarcoHuXHu <huxhu2333@gmail.com>
2017-10-24 20:19:52 +0800

    finish register

--- web/apis.py
+++ web/apis.py
@@ -24,12 +24,13 @@ async def registerUser(*, username, password, email, phone):
     _RE_EMAIL = re.compile(r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$')
     _RE_SHA1 = re.compile(r'^[0-9a-f]{40}$')
     username = username.strip()
+    password = password.strip()
+    email = email.strip()
+    phone = phone.strip()
     if not username:
         raise APIError('username')
-    password = password.strip()
-    if not password:# or not _RE_SHA1.match(password):
+    if not password or not _RE_SHA1.match(password):
         raise APIError('password')
-    email = email.strip()
     if not email or not _RE_EMAIL.match(email):
         raise APIError('email')
     users = await User.find('username=? or email=?', [username, email])
@@ -38,5 +39,5 @@ async def registerUser(*, username, password, email, phone):
     sp = hashlib.sha1()
     sp.update(password.encode('utf-8'))
     user = User(username=username, email=email, password=sp.hexdigest(), phone=phone)
-    # await user.save()
+    await user.save()
     return dict(user=user)
